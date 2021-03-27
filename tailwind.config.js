da128bbc068038bb55ee6385df06e1b55d7edd13
Will Taylor <wtaylor151@gmail.com>
2021-03-27 10:08:08 -0500

    get website working

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  theme: {
+    screens: {
+      sm: '640px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1440px',
+    },
+    extend: {
+      colors: {
+        bpBotPurple: '#4151F6'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
